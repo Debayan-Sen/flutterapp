d638913e1619c9c2b5db6f0e57c1ad698d1ba504
Debayan <sdebayan999@gmail.com>
2022-09-28 22:15:37 +0530

    different widgets

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,81 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: Text("My Flutter App")),
         body:  Container(
-          height: 200,
-          width: 200,
-          color: Colors.black26,
-          margin: EdgeInsets.all(20),
+          height: 500,
+          width: 400,
+          padding: EdgeInsets.all(20),
+          color: Colors.black12,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.amber,
+                  child: Icon(Icons.supervised_user_circle_outlined,
+                    size: 60,
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.green,
+                )
+
+              ],),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.amber,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.green,
+                )
+
+              ],),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.amber,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.green,
+                )
+
+              ],),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {
           print("Henlo!!");
