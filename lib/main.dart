0e7534cc2c6c36ad536a8ffc0b5de69fe747511b
Debayan <sdebayan999@gmail.com>
2022-10-08 08:21:35 +0530

    listView builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// import 'dart:html';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +16,33 @@ class MyApp extends StatefulWidget {
 
 class  MyAppState extends State <MyApp> {
 
-  String name = "Debayan";
+  List data = [
+    {
+      "image" : "https://images.pexels.com/photos/38136/pexels-photo-38136.jpeg?auto=compress&cs=tinysrgb&w=1260&h=300&dpr=1",
+      "name" : "Forest",
+      "likes" : 152
+    },
+    {
+      "image" : "https://images.pexels.com/photos/1033729/pexels-photo-1033729.jpeg?auto=compress&cs=tinysrgb&w=1260&h=300&dpr=1",
+      "name" : "Road",
+      "likes" : 254
+    },
+    {
+      "image" : "https://images.pexels.com/photos/414144/pexels-photo-414144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=300&dpr=1",
+      "name" : "NightSky",
+      "likes" : 420
+    },
+    {
+      "image" : "https://images.pexels.com/photos/247431/pexels-photo-247431.jpeg?auto=compress&cs=tinysrgb&w=1260&h=300&dpr=1",
+      "name" : "Elephant",
+      "likes" : 211
+    },
+    {
+      "image" : "https://images.pexels.com/photos/237272/pexels-photo-237272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=300&dpr=1",
+      "name" : "Beach",
+      "likes" : 655
+    }
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -22,23 +51,43 @@ class  MyAppState extends State <MyApp> {
         appBar: AppBar(
           title: Text("My Flutter App"),
         ),
-        body: Container(
+        body: ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (context, index){
+            return Container(
           padding: EdgeInsets.all(20),
-          child: Text(name, style: TextStyle(fontSize: 50),),
+          height: 400,
+          color: Colors.black12,
+          child: Column(children: [
+            Image.network(data[index]["image"]),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(data[index]["name"], style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),),
+                 Row(
+                  children: [
+                    Icon(Icons.favorite),
+                    SizedBox(width: 5,),
+                    Text(data[index]["likes"].toString(),style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),),
+                    SizedBox(width: 10,),
+                    Icon(Icons.comment),
+
+                  ],
+                 )
+              ],
+            )
+          ]),
+        );
+          },
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            setState(() {
-              if (name == "Dev") {
-                name = "Debayan";
-              }else{
-                name ="Dev";
-              }
-            });
+            
           },
           child: Icon(Icons.change_circle),
         ),
-      ),
+      ),  
     );
   }
 }
